deb6f2a922619f9dc2932e9fcf5bfa77e4746131
kawindme <32780200+kawindme@users.noreply.github.com>
2022-01-04 15:11:09 +0100

    Increase air speed to see if this improves sending frequency.

--- loraconfig.py
+++ loraconfig.py
@@ -67,6 +67,7 @@ lora_hat_default = {
 
 lora_hat_config = lora_hat_default.copy()
 lora_hat_config["enable_point_to_point_mode"] = True
+lora_hat_config["air_speed"] = AirSpeed.AS_62_5K
 
 # (Partly) overwrite with localconfig
 try:
